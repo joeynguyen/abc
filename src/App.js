4e2a786297a9497ecda6b685c7d8c5319fc9b356
Joey Nguyen <jnn193@gmail.com>
2020-11-15 00:04:17 -0600

    Add custom hooks for capturing key down and up events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
 import './App.css';
 
-function App() {
+import { useState, useEffect } from 'react';
+
+// Usage
+export default function App() {
+  const keyPressed = useKeyPressListener();
+  console.log("keyPressed", keyPressed);
+  // Call our hook for each key that we'd like to monitor
+  const happyPress = useKeyPress('h');
+  const sadPress = useKeyPress('s');
+  const robotPress = useKeyPress('r');
+  const foxPress = useKeyPress('f');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>h, s, r, f</div>
+      <div>
+        {happyPress && '😊'}
+        {sadPress && '😢'}
+        {robotPress && '🤖'}
+        {foxPress && '🦊'}
+      </div>
     </div>
   );
 }
 
-export default App;
+// Hook
+function useKeyPress(targetKey) {
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  // If pressed key is our target key then set to true
+  const downHandler = useCallback(({ key }) => {
+    if (key === targetKey) {
+      setKeyPressed(true);
+    }
+  }, [targetKey]);
+
+  // If released key is our target key then set to false
+  const upHandler = useCallback(({ key }) => {
+    if (key === targetKey) {
+      setKeyPressed(false);
+    }
+  }, [targetKey]);
+
+  // Add event listeners
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler);
+    window.addEventListener('keyup', upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+      window.removeEventListener('keyup', upHandler);
+    };
+  }, [downHandler, upHandler]); // Empty array ensures that effect is only run on mount and unmount
+
+  return keyPressed;
+}
+
+function useKeyPressListener() {
+
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState('');
+
+  // If pressed key is our target key then set to true
+  const downHandler = useCallback(({ key }) => {
+    setKeyPressed(key);
+  }, []);
+
+  // Add event listeners
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+    };
+  }, [downHandler]); // Empty array ensures that effect is only run on mount and unmount
+
+  return keyPressed;
+}
