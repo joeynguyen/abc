902a7955c4dfc2831e4185b515e28694384306b3
Joey Nguyen <jnn193@gmail.com>
2020-11-21 00:05:38 -0600

    update styling, add function for getting coordinates for characters

--- src/App.js
+++ src/App.js
@@ -1,78 +1,31 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import './App.css';
 
-import { useState, useEffect } from 'react';
-
-const fontSize = 100;
-const viewportWidth = document.documentElement.clientWidth;
-console.log("viewportWidth", viewportWidth);
-const viewportHeight = document.documentElement.clientHeight;
-console.log("viewportHeight", viewportHeight);
-
-// Usage
 export default function App() {
-  const keyPressed = useKeyPressListener();
+  const keyPressed = useKeyPress();
   // const modifierKeys = ["Alt", "Shift", "Meta", "Control"]
   // const ignoredKeys = ["Escape"]
   const acceptedKeysRegex = /[A-Za-z0-9]/; // only English alphabets and numbers
 
-  let top = getRandomInt(0, viewportHeight);
-  let left = getRandomInt(0, viewportWidth);
-
-  // make sure the character doesn't show up outside of the viewport
-  if (viewportHeight - top < fontSize) {
-    top = top - (fontSize + 100);
-  }
-  if (viewportWidth - left < fontSize) {
-    left = left - (fontSize + 100);
-  }
+  const coords = getCoordinates();
 
   return (
-    <div style={{
-      position: 'absolute',
-      top,
-      left,
-      fontSize: `${fontSize}px`,
-    }}>
-      {keyPressed.length === 1 && acceptedKeysRegex.test(keyPressed) ? keyPressed : ''}
+    <div
+      className="char"
+      style={{
+        top: coords.top,
+        left: coords.left,
+      }}>
+      {
+        keyPressed.length === 1 && acceptedKeysRegex.test(keyPressed)
+          ? keyPressed
+          : ''
+      }
     </div>
   );
 }
 
-// Hook
-function useKeyPress(targetKey) {
-  // State for keeping track of whether key is pressed
-  const [keyPressed, setKeyPressed] = useState(false);
-
-  // If pressed key is our target key then set to true
-  const downHandler = useCallback(({ key }) => {
-    if (key === targetKey) {
-      setKeyPressed(true);
-    }
-  }, [targetKey]);
-
-  // If released key is our target key then set to false
-  const upHandler = useCallback(({ key }) => {
-    if (key === targetKey) {
-      setKeyPressed(false);
-    }
-  }, [targetKey]);
-
-  // Add event listeners
-  useEffect(() => {
-    window.addEventListener('keydown', downHandler);
-    window.addEventListener('keyup', upHandler);
-    // Remove event listeners on cleanup
-    return () => {
-      window.removeEventListener('keydown', downHandler);
-      window.removeEventListener('keyup', upHandler);
-    };
-  }, [downHandler, upHandler]); // Empty array ensures that effect is only run on mount and unmount
-
-  return keyPressed;
-}
-
-function useKeyPressListener() {
+function useKeyPress() {
   // State for keeping track of whether key is pressed
   const [keyPressed, setKeyPressed] = useState('');
 
@@ -96,5 +49,28 @@ function useKeyPressListener() {
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const fontSize = 100;
+const viewportWidth = document.documentElement.clientWidth;
+const viewportHeight = document.documentElement.clientHeight;
+
+function getCoordinates() {
+  let top = getRandomInt(0, viewportHeight);
+  let left = getRandomInt(0, viewportWidth);
+
+  // make sure the character doesn't show up outside of the viewport
+  if (viewportHeight - top < fontSize) {
+    top = top - (fontSize * 2.1);
+  }
+  if (viewportWidth - left < fontSize) {
+    left = left - (fontSize * 2.1);
+  }
+
+  return {
+    top,
+    left,
+  };
 }
